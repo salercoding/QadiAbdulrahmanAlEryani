7516ef5d1a980c85a899deacd288a811f28037f4
Selwan Al-Eryani <selwanaleryani@gmail.com>
2023-12-26 17:54:01 -0500

    provided link to pdf of first memoir

--- src/components/memoirs.js
+++ src/components/memoirs.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Memoirs = () => {
+import PartOne from "./memoir-part-one.pdf";
+
+class Memoirs extends Component {
+    render() {
+        return (
+            <div className = "App">
+              <a href = {PartOne} target = "_blank"><img src="images/memoir-one-image.jpeg"/></a>
+            </div>
+        );
+}
+}
+/*const Memoirs = () => {
     return (
         <div className="memoirs">
         <h2> Memoirs </h2>
-        <a href="linktopdf"><img src="sample-image.jpeg"></img></a>
+      <img src="images/memoir-one-image.jpeg"/>
         <a href="linktopdf"><img src="sample-image.jpeg"></img></a>
         <a href="linktopdf"><img src="sample-image.jpeg"></img></a>
         </div>
     );
-}
+}*/
 
 export default Memoirs;
