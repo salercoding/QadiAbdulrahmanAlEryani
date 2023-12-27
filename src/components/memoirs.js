d0044d7e6aba007cdc32d4ce79599ba79dc9d69e
Selwan Al-Eryani <selwanaleryani@gmail.com>
2023-12-27 13:00:02 -0500

    uploaded links to all three memoirs on memoirs page

--- src/components/memoirs.js
+++ src/components/memoirs.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 
 import PartOne from "./memoir-part-one.pdf";
-
+import PartTwo from "./memoir-part-two.pdf";
+import PartThree from "./memoir-part-three.pdf";
 class Memoirs extends Component {
     render() {
         return (
             <div className = "App">
-              <a href = {PartOne} target = "_blank"><img src="images/memoir-one-image.jpeg"/></a>
+              <a href = {PartOne} target = "_blank"><img src="images/memoir-part-one-image.jpg"/></a>
+              <a href = {PartTwo} target = "_blank"><img src="images/memoir-part-two-image.jpg"/></a>
+              <a href = {PartThree} target = "_blank"><img src="images/memoir-part-three-image.jpg"/></a>
             </div>
         );
 }
 }
-/*const Memoirs = () => {
-    return (
-        <div className="memoirs">
-        <h2> Memoirs </h2>
-      <img src="images/memoir-one-image.jpeg"/>
-        <a href="linktopdf"><img src="sample-image.jpeg"></img></a>
-        <a href="linktopdf"><img src="sample-image.jpeg"></img></a>
-        </div>
-    );
-}*/
-
 export default Memoirs;
